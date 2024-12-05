f59b8c468afe7234fd32b1c71af6f4bf51f112fe
Henrique Furtado <henriquefurtadosilva@gmail.com>
2024-12-05 13:55:34 -0400

    API

--- Nao-Sei-Bar-Backend/Nao-Sei-Bar-Backend/obj/Debug/net8.0/Nao-Sei-Bar-Backend.AssemblyInfo.cs
+++ Nao-Sei-Bar-Backend/Nao-Sei-Bar-Backend/obj/Debug/net8.0/Nao-Sei-Bar-Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Nao-Sei-Bar-Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d300001275676f87e4d3999c531907ddc2d2f6e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57412166b6cf07dece05c09764589327ff468f53")]
 [assembly: System.Reflection.AssemblyProductAttribute("Nao-Sei-Bar-Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Nao-Sei-Bar-Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
